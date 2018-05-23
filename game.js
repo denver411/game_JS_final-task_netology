ec48819193ae8a5bd4ff9891b8b17582b93245e4
Denis Zhebov <zhebov@gmail.com>
2018-05-23 18:39:58 +0300

    Добавил класс Vector

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
 'use strict';
+
+class Vector {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+  plus(obj) {
+    if (!(obj instanceof Vector)) {
+      throw new Error("Можно прибавлять к вектору только вектор типа Vector");
+    } else {
+      return new Vector(this.x + obj.x, this.y + obj.y);
+    }
+  }
+  times(scale) {
+    return new Vector(this.x * scale, this.y * scale);
+  }
+}
